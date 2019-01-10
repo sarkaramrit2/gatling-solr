08d008b8a7295dd39d5214616b1b7f2bf25321b0
sarkaramrit2 <amrit@ebctechnologies.com>
2019-01-10 23:40:35 +0530

    adding jenkins k8s support

--- jenkins/k8s/kubectl-cluster.sh
+++ jenkins/k8s/kubectl-cluster.sh
@@ -26,5 +26,6 @@ docker exec kubectl_support gcloud config get-value core/account
 docker exec kubectl_support gcloud config set project strange-team-223300
 docker exec kubectl_support gcloud config set compute/zone us-central1-a
 docker exec kubectl_support gcloud auth configure-docker
+echo "y"
 docker exec kubectl_support gcloud config list
 docker exec kubectl_support gcloud container clusters get-credentials solr-cluster
