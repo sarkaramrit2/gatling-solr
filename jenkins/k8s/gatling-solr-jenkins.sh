b041acbccede8c6a9a99243a266b4facae44bbb2
sarkaramrit2 <amrit@ebctechnologies.com>
2019-01-11 01:03:00 +0530

    slight modofications

--- jenkins/k8s/gatling-solr-jenkins.sh
+++ jenkins/k8s/gatling-solr-jenkins.sh
@@ -25,9 +25,10 @@ do
    TOTAL_PODS=`docker exec kubectl_support kubectl get pods --field-selector=status.phase=Running --namespace=jenkins | wc -l`
 done
 
-# create collection 'wiki' with shards 2 replicas 2
+# (re)create collection 'wiki' with shards 2 replicas 2
 docker cp ./jenkins/collection-config ${CID}:/opt/collection-config
 docker exec kubectl_support kubectl cp /opt/collection-config jenkins/solr-dummy-cluster-0:/opt/solr/collection-config
+docker exec kubectl_support kubectl exec -n jenkins solr-dummy-cluster-0 -- /opt/solr/bin/solr delete -c wiki
 docker exec kubectl_support kubectl exec -n jenkins solr-dummy-cluster-0 -- /opt/solr/bin/solr create -c wiki -s 2 -rf 2 -d /opt/solr/collection-config/
 
 # optional property files a user may have uploaded to jenkins
