870ebc6d874b26bc20894ae631ec2e9169a5360d
sarkaramrit2 <amrit@ebctechnologies.com>
2019-01-11 00:21:31 +0530

    adding jenkins k8s support

--- jenkins/k8s/clean-k8s.sh
+++ jenkins/k8s/clean-k8s.sh
@@ -8,10 +8,10 @@ if [ ! -z "${CID}" ]; then
     docker exec kubectl_support kubectl delete namespaces jenkins
 
 # check status of the pods in every 30 seconds
-    PODS_STATUS=`echo `docker exec kubectl_support kubectl get pods --namespace=jenkins``
+    PODS_STATUS=`docker exec kubectl_support kubectl get pods --namespace=jenkins`
     while [ "${PODS_STATUS}" != "No resources found." ]
     do
         sleep 30
-        PODS_STATUS=`echo `docker exec kubectl_support kubectl get pods --namespace=jenkins``
+        PODS_STATUS=`docker exec kubectl_support kubectl get pods --namespace=jenkins`
     done
 fi
