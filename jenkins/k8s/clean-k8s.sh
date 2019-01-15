d892482d5760ebbc4cab159cee5adc0d938d5eba
sarkaramrit2 <amrit@ebctechnologies.com>
2019-01-16 01:34:38 +0530

    --

--- jenkins/k8s/clean-k8s.sh
+++ jenkins/k8s/clean-k8s.sh
@@ -2,7 +2,9 @@
 
 if [ "$IMPLICIT_CLUSTER" = true ] ; then
     CID=`docker container ls -aq -f "name=kubectl-support"`
-    if [ ! -z "${CID}" ]; then
+    # extra layer of check to make sure namespace with jenkins gets deleted
+    # TODO: remove this hardcoded check for future
+    if [ ! -z "${CID}" -a "${GCP_K8_CLUSTER_NAMESPACE}" == "jenkins" ]; then
     # delete pods
         docker exec kubectl-support kubectl delete --all pods --namespace=${GCP_K8_CLUSTER_NAMESPACE}
     # remove namespace
