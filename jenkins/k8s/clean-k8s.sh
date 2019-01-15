a14a09649c2711c00990227cc8b0371c250b11db
sarkaramrit2 <amrit@ebctechnologies.com>
2019-01-15 18:56:21 +0530

    add namespace param

--- jenkins/k8s/clean-k8s.sh
+++ jenkins/k8s/clean-k8s.sh
@@ -3,15 +3,15 @@
 CID=`docker container ls -aq -f "name=kubectl-support"`
 if [ ! -z "${CID}" ]; then
 # delete pods
-    docker exec kubectl-support kubectl delete --all pods --namespace=jenkins
+    docker exec kubectl-support kubectl delete --all pods --namespace=${GCP_K8_CLUSTER_NAMESPACE}
 # remove namespace
-    docker exec kubectl-support kubectl delete namespaces jenkins
+    docker exec kubectl-support kubectl delete namespaces ${GCP_K8_CLUSTER_NAMESPACE}
 
 # check status of the pods in every 30 seconds
-    PODS_STATUS=`docker exec kubectl-support kubectl get pods --namespace=jenkins`
+    PODS_STATUS=`docker exec kubectl-support kubectl get pods --namespace=${GCP_K8_CLUSTER_NAMESPACE}`
     while [ ! -z "${PODS_STATUS}" ]
     do
         sleep 30
-        PODS_STATUS=`docker exec kubectl-support kubectl get pods --namespace=jenkins`
+        PODS_STATUS=`docker exec kubectl-support kubectl get pods --namespace=${GCP_K8_CLUSTER_NAMESPACE}`
     done
 fi
