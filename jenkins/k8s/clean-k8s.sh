8d7580e3a013ace52d310b08b616a175e01085a5
sarkaramrit2 <amrit@ebctechnologies.com>
2019-01-15 23:32:46 +0530

    ---

--- jenkins/k8s/clean-k8s.sh
+++ jenkins/k8s/clean-k8s.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env bash
 
-CID=`docker container ls -aq -f "name=kubectl-support"`
-if [ ! -z "${CID}" ]; then
-# delete pods
-    docker exec kubectl-support kubectl delete --all pods --namespace=${GCP_K8_CLUSTER_NAMESPACE}
-# remove namespace
-    docker exec kubectl-support kubectl delete namespaces ${GCP_K8_CLUSTER_NAMESPACE}
+if [ "$IMPLICIT_CLUSTER" = true ] ; then
+    CID=`docker container ls -aq -f "name=kubectl-support"`
+    if [ ! -z "${CID}" ]; then
+    # delete pods
+        docker exec kubectl-support kubectl delete --all pods --namespace=${GCP_K8_CLUSTER_NAMESPACE}
+    # remove namespace
+        docker exec kubectl-support kubectl delete namespaces ${GCP_K8_CLUSTER_NAMESPACE}
 
-# check status of the pods in every 30 seconds
-    PODS_STATUS=`docker exec kubectl-support kubectl get pods --namespace=${GCP_K8_CLUSTER_NAMESPACE}`
-    while [ ! -z "${PODS_STATUS}" ]
-    do
-        sleep 30
+    # check status of the pods in every 30 seconds
         PODS_STATUS=`docker exec kubectl-support kubectl get pods --namespace=${GCP_K8_CLUSTER_NAMESPACE}`
-    done
+        while [ ! -z "${PODS_STATUS}" ]
+        do
+            sleep 30
+            PODS_STATUS=`docker exec kubectl-support kubectl get pods --namespace=${GCP_K8_CLUSTER_NAMESPACE}`
+        done
+    fi
 fi
