38728e7f0c6f3cd16199f3ab62399b3fc85ab753
sarkaramrit2 <amrit@ebctechnologies.com>
2019-01-15 23:22:47 +0530

    --

--- jenkins/k8s/gatling-solr-jenkins.sh
+++ jenkins/k8s/gatling-solr-jenkins.sh
@@ -70,6 +70,12 @@ if [ "$IMPLICIT_CLUSTER" = true ] ; then
     docker exec kubectl-support kubectl cp /opt/collection-config ${GCP_K8_CLUSTER_NAMESPACE}/solr-dummy-cluster-0:/opt/solr/collection-config
     docker exec kubectl-support kubectl exec -n ${GCP_K8_CLUSTER_NAMESPACE} solr-dummy-cluster-0 -- /opt/solr/bin/solr delete -c wiki || echo "create collection now"
     docker exec kubectl-support kubectl exec -n ${GCP_K8_CLUSTER_NAMESPACE} solr-dummy-cluster-0 -- /opt/solr/bin/solr create -c wiki -s 2 -rf 2 -d /opt/solr/collection-config/
+else
+        # (re)create collection 'wiki' with shards 2 replicas 2
+    docker cp ./jenkins/collection-config ${CID}:/opt/collection-config
+    docker exec kubectl-support kubectl cp /opt/collection-config ${GCP_K8_CLUSTER_NAMESPACE}/solr-solr-bench-0:/opt/solr/collection-config
+    docker exec kubectl-support kubectl exec -n ${GCP_K8_CLUSTER_NAMESPACE} solr-solr-bench-0 -- /opt/solr/bin/solr delete -c wiki || echo "create collection now"
+    docker exec kubectl-support kubectl exec -n ${GCP_K8_CLUSTER_NAMESPACE} solr-solr-bench-0 -- /opt/solr/bin/solr create -c wiki -s 2 -rf 2 -d /opt/solr/collection-config/
 fi
 
 # optional property files a user may have uploaded to jenkins
