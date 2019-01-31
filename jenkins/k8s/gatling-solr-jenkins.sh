bb85b94c60ee24b4e29f155cd75a690768ea47f0
sarkaramrit2 <amrit@ebctechnologies.com>
2019-02-01 00:43:54 +0530

    ---

--- jenkins/k8s/gatling-solr-jenkins.sh
+++ jenkins/k8s/gatling-solr-jenkins.sh
@@ -73,7 +73,7 @@ else
     # wait until all pods comes up running
     TOTAL_PODS=`docker exec kubectl-support kubectl get pods --field-selector status.phase=Running --field-selector metadata.name=gatling-solr-0 --namespace=${GCP_K8_CLUSTER_NAMESPACE} | wc -l`
     # find better way to determine all pods running
-    while [ "${TOTAL_PODS}" != "2" ]
+    while [ "${TOTAL_PODS}" != "${ESTIMATED_NODES_1}" -a "${TOTAL_PODS}" != "${ESTIMATED_NODES_2}" ]
     do
        sleep 30
        TOTAL_PODS=`docker exec kubectl-support kubectl get pods --field-selector status.phase=Running --field-selector metadata.name=gatling-solr-0 --namespace=${GCP_K8_CLUSTER_NAMESPACE} | wc -l`
