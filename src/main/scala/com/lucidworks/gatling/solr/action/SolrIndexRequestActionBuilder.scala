9cffb59f7a0d10a5bcd077fafd3377719c264eca
sarkaramrit2 <amrit@ebctechnologies.com>
2019-02-08 15:39:31 +0530

    add header support

--- src/main/scala/com/lucidworks/gatling/solr/action/SolrIndexRequestActionBuilder.scala
+++ src/main/scala/com/lucidworks/gatling/solr/action/SolrIndexRequestActionBuilder.scala
@@ -7,6 +7,9 @@ import com.lucidworks.gatling.solr.request.builder.SolrIndexAttributes
 import io.gatling.core.action.Action
 import io.gatling.core.action.builder.ActionBuilder
 import io.gatling.core.structure.ScenarioContext
+import org.apache.http.client.config.RequestConfig
+import org.apache.http.impl.client.HttpClients
+import org.apache.http.message.BasicHeader
 import org.apache.solr.client.solrj.impl.CloudSolrClient
 
 
@@ -17,15 +20,20 @@ class SolrIndexRequestActionBuilder[K, V](solrAttributes: SolrIndexAttributes[K,
 
     val solrComponents: SolrComponents = protocolComponentsRegistry.components(SolrProtocol.SolrProtocolKey)
 
+    val httpBuilder = HttpClients.custom()
+    httpBuilder.setDefaultHeaders(util.Arrays.asList(new BasicHeader("X-Default-Header", "default header")))
+    httpBuilder.setDefaultRequestConfig(RequestConfig.custom.setExpectContinueEnabled(true).build())
+    val httpClient = httpBuilder.build()
+
     val solrClients = new util.ArrayList[CloudSolrClient]()
 
     for( i <- 0 until solrComponents.solrProtocol.numClients){
       var solrClient= null: CloudSolrClient;
       if (solrComponents.solrProtocol.zkhost != null & !solrComponents.solrProtocol.zkhost.isEmpty) {
-        solrClient = new CloudSolrClient.Builder().withZkHost(solrComponents.solrProtocol.zkhost).build()
+        solrClient = new CloudSolrClient.Builder().withZkHost(solrComponents.solrProtocol.zkhost).withHttpClient(httpClient).build()
       }
       else if (solrComponents.solrProtocol.solrurl != null & !solrComponents.solrProtocol.solrurl.isEmpty) {
-        solrClient = new CloudSolrClient.Builder().withSolrUrl(solrComponents.solrProtocol.solrurl).build()
+        solrClient = new CloudSolrClient.Builder().withSolrUrl(solrComponents.solrProtocol.solrurl).withHttpClient(httpClient).build()
       }
       solrClient.setDefaultCollection(solrComponents.solrProtocol.collection)
       solrClients.add(solrClient)
