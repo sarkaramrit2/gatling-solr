a2f667e1037ce4be2490d60e088a3c558e723b9b
sarkaramrit2 <sarkaramrit2@gmail.com>
2019-11-02 15:39:03 +0530

    add resource limits to gatling

--- refresh.sh
+++ refresh.sh
@@ -1,4 +1,4 @@
 rm -rf target/ 
 sbt assembly 
-cp /Users/apple/git_space/gatling-solr/gatling-solr/target/scala-2.12/gatling-solr* ~/Work/GATLING/lib/
-cp /Users/apple/git_space/gatling-solr/gatling-solr/src/test/scala/* /Users/apple/Work/GATLING/user-files/simulations/
+cp /Users/apple/git_space/gatling-solr/gatling-solr/target/scala-2.12/gatling-solr* /Volumes/A-HD/Work/GATLING/lib/
+cp /Users/apple/git_space/gatling-solr/gatling-solr/src/test/scala/* /Volumes/A-HD/Work/GATLING/user-files/simulations/
