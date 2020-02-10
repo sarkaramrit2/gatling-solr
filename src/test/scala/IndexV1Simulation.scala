3dc4e4086e7de9e7067577a9740a9ed1e8b52ec7
sarkaramrit2 <sarkaramrit2@gmail.com>
2020-02-10 13:13:16 +0530

    updated simulations

--- src/test/scala/IndexV1Simulation.scala
+++ src/test/scala/IndexV1Simulation.scala
@@ -1,10 +1,12 @@
 import java.io.{File, FileReader}
+import java.net.URL
 import java.util.{Properties, Scanner}
 
 import lucidworks.gatling.solr.Predef._
 import io.gatling.core.Predef._
 import io.gatling.core.feeder.Feeder
 import org.apache.solr.client.solrj.impl.CloudSolrClient
+
 import scala.util.control.Breaks._
 
 class IndexV1Simulation extends Simulation {
@@ -15,22 +17,25 @@ class IndexV1Simulation extends Simulation {
 
     val prop: Properties = new Properties
     val propFile = new FileInputStream("/opt/gatling/user-files/" +
-      "configs/index.1.config.properties")
+      "configs/index.config.properties")
     prop.load(propFile)
 
     val indexFilePath = prop.getProperty("indexFilePath", "/opt/gatling/user-files/" +
-      "data/enwiki.random.lines.csv")
-    val numBatchesPerUser = prop.getProperty("numBatchesPerUser", "200")
-    val maxNumUsers = prop.getProperty("maxNumUsers", "2")
+      "data/enwiki.random.lines.csv.txt")
+    val numBatchesPerUser = prop.getProperty("numBatchesPerUser", "1")
+    val maxNumUsers = prop.getProperty("maxNumUsers", "1")
     val minNumUsers = prop.getProperty("minNumUsers", "1")
-    val totalTimeInMinutes = prop.getProperty("totalTimeInMinutes", "1")
+    val totalTimeInMinutes = prop.getProperty("totalTimeInMinutes", "34")
     val indexBatchSize = prop.getProperty("indexBatchSize", "5000")
     val zkHost = prop.getProperty("zkHost", "localhost:9983")
-    val solrUrl = prop.getProperty("solrUrl", "http://localhost:8983/solr")
-    val apiKey = prop.getProperty("apiKey", "--empty-here--")
+    val solrUrl = prop.getProperty("solrUrl", "http://localhost:8983/solr/")
     val defaultCollection = prop.getProperty("defaultCollection", "wiki")
-    val header = prop.getProperty("header", "title,time,description")
-    val numClients = prop.getProperty("numClients", "1")
+    val header = prop.getProperty("header", "id,title,time,description")
+    val headerSep = prop.getProperty("header.sep", ",")
+    val multiParamSep = prop.getProperty("multiParam.sep", null)
+    val fieldValuesSep = prop.getProperty("fieldValues.sep", ",")
+    val numClients = prop.getProperty("numClients", "9")
+    val oauth2CustomerId = prop.getProperty("CUSTOMER_ID", "lucidworks")
     val parallelNodes = prop.getProperty("parallelNodes", "1")
     val totalFiles = prop.getProperty("totalFiles", "1")
     val podNo = if (System.getenv("POD_NAME") != null) {
@@ -45,17 +50,37 @@ class IndexV1Simulation extends Simulation {
 
     private var podNo = Config.podNo.toInt
     private var indexFile: File = _
+    private var url: URL = _
     if (Config.totalFiles.toInt <= 1) {
-      System.out.println("indexFile: " + Config.indexFilePath)
-      indexFile = new File(Config.indexFilePath)
+      if (Config.indexUrlPath != null) {
+        System.out.println("indexUrl: " + Config.indexUrlPath)
+        url = new URL(Config.indexUrlPath)
+      }
+      else {
+        System.out.println("indexFile: " + Config.indexFilePath)
+        indexFile = new File(Config.indexFilePath)
+      }
     }
     else {
-      System.out.println("indexFile: " + Config.indexFilePath + Config.podNo)
-      indexFile = new File(Config.indexFilePath + Config.podNo)
+      if (Config.indexUrlPath != null) {
+        System.out.println("indexUrl: " + Config.indexUrlPath + Config.podNo)
+        url = new URL(Config.indexUrlPath + Config.podNo)
+      }
+      else {
+        System.out.println("indexFile: " + Config.indexFilePath + Config.podNo)
+        indexFile = new File(Config.indexFilePath + Config.podNo)
+      }
     }
 
-    private var fileReader = new FileReader(indexFile)
-    private var scanner = new Scanner(fileReader)
+    private var fileReader: FileReader = _
+    private var scanner: Scanner = _
+    if (Config.indexUrlPath != null) {
+      scanner = new Scanner(url.openStream())
+    }
+    else {
+      fileReader = new FileReader(indexFile)
+      scanner = new Scanner(fileReader)
+    }
 
     override def hasNext = if (!scanner.hasNext) {
       if (Config.totalFiles.toInt <= 1) {
@@ -66,10 +91,20 @@ class IndexV1Simulation extends Simulation {
           false
         }
         else {
+          scanner.close()
+          if (Config.indexUrlPath == null) {
+            fileReader.close()
+          }
           podNo = podNo + Config.parallelNodes.toInt
-          indexFile = new File(Config.indexFilePath + Config.podNo)
-          fileReader = new FileReader(indexFile)
-          scanner = new Scanner(fileReader)
+          if (Config.indexUrlPath != null) {
+            url = new URL(Config.indexUrlPath + Config.podNo)
+            scanner = new Scanner(url.openStream())
+          }
+          else {
+            indexFile = new File(Config.indexFilePath + Config.podNo)
+            fileReader = new FileReader(indexFile)
+            scanner = new Scanner(fileReader)
+          }
           true
         }
       }
