4e45ba92654e82bf8e5d16af5fa040b1a2eb7ac1
sarkaramrit2 <sarkaramrit2@gmail.com>
2020-08-17 14:59:30 +0530

    --

--- jenkins/k8s/clean-docker.sh
+++ jenkins/k8s/clean-docker.sh
@@ -1,19 +1,20 @@
 #!/usr/bin/env bash
 
 # stop containers
-CID=`docker container ls -aq -f "name=kubectl-support"`
+KUBECTL_SUPPORT=kubectl-support-${GCP_K8_CLUSTER_NAMESPACE}
+CID=`docker container ls -aq -f "name=${KUBECTL_SUPPORT}"`
 if [ ! -z "${CID}" ]; then
     docker container stop ${CID}
 fi
 
 #remove containers
-CID=`docker container ls -aq -f "name=kubectl-support"`
+CID=`docker container ls -aq -f "name=${KUBECTL_SUPPORT}"`
 if [ ! -z "${CID}" ]; then
     docker container rm ${CID}
 fi
 
 # remove all gatling solr dockers
-IMG_ID=`docker images -a | grep "kubectl-support" | awk '{print $3}'`
+IMG_ID=`docker images -a | grep "${KUBECTL_SUPPORT}" | awk '{print $3}'`
 if [ ! -z "${IMG_ID}" ]; then
     docker rmi -f ${IMG_ID}
 fi
